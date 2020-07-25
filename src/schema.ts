6c73b4e36f1455eb41b9d1d7f454fae26854cf7f
Bryan Walker <bryan.walker@revunit.com>
2020-07-25 14:43:34 -0600

    initial commit

---
+++ src/schema.ts
@@ -0,0 +1,13 @@
+// schema.ts
+import "graphql-import-node";
+import * as typeDefs from "./schema/schema.graphql";
+import { makeExecutableSchema } from "graphql-tools";
+import resolvers from "./resolverMap";
+import { GraphQLSchema } from "graphql";
+
+const schema: GraphQLSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+export default schema;
